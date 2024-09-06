f36306ddc2e0d5c5a2ceaf3e68d183a41f60a016
touhidul-islam-html <ti360109@gmail.com>
2024-09-06 18:04:37 +0600

    images stored to public

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+
 const Card = ({ data }) => {
   return (
     <div className="">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-6 px-6 lg:px-40 mx-auto">
-        {data.map((service, index) => (
+        {data.map((data, index) => (
           <div
             key={index}
-            value={service}
+            value={data}
             className="p-4  border-[1px] border-gray-800 rounded-2xl"
           >
             <div className="flex rounded-2xl h-full p-2 flex-col">
@@ -15,17 +17,17 @@ const Card = ({ data }) => {
                 <div className="inline-flex items-center justify-center flex-shrink-0 p-6">
                   <img
                     className="lg:w-[200px] w-[150px]"
-                    src={service.imageUrl}
-                    alt={service.name}
+                    src={data.imageUrl}
+                    alt={data.name}
                   />
                 </div>
               </div>
               <div className="flex flex-col justify-center items-center flex-grow">
                 <p className="leading-relaxed mb-4 text-[26px] bg-gradient-to-b from-white to-gray-500 text-transparent font-bold bg-clip-text">
-                  {service.name}
+                  <Link to={data.url}>{data.name}</Link>
                 </p>
                 <p className="leading-relaxed text-white text-[15px] text-center">
-                  {service.description}
+                  {data.description}
                 </p>
               </div>
             </div>
